595a7b672cd9f6dff3e894c5fad8e0eec8aec0c9
Stephen Callum <stephen.callum@hotmail.co.uk>
2020-01-20 14:41:57 +0000

    Adding labs folder

---
+++ Labs/Lab_05_LogToEventViewer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Lab_05_LogToEventViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EventLog.WriteEntry("Application", "Your system is about to expire", EventLogEntryType.Error, 5000, 1234);
+        }
+    }
+}
